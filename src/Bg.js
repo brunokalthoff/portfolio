89af12933fa58fe06b8cafba042b4bee92563c38
ethbru <ethbru@outlook.de>
2022-06-01 17:14:24 +0800

    Color palette

--- src/Bg.js
+++ src/Bg.js
@@ -11,13 +11,12 @@ function Bg() {
 
     return (
         <>
+        <div className="ball1"></div>
         <motion.div id="cloud1" initial={{x:0}} animate={{x:20}} transition={{duration: 20, repeat: Infinity, repeatType:"mirror", ease:"linear"}}>{cloud1}</motion.div>
         <motion.div id="cloud3">{cloud1}</motion.div>
        <div id="cloud2">{cloud2}</div>
-       <div id="cloud4">{cloud2}</div> 
-
-
-
+       <div id="cloud4">{cloud1}</div> 
+       <div id="cloud5">{cloud1}</div> 
 
             <motion.div animate={{y:"-1000%"}} transition={{ repeat: Infinity, repeatType: "loop", duration: 240, ease: "linear", repeatDelay: 0 }} className="circle">
 
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -11,38 +11,67 @@ function ContactForm() {
     const [buttonBg, setButtonBg] = useState('var(--lightBlue)');
     const [buttonVal, setButtonVal] = useState('Send');
 
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        setButtonVal('...');
-
-        setTimeout(() => {
-            setName('');
-            setEmail('');
-            setPhone('');
-            setNote('');
-            setButtonVal('Message sent!')
-            setButtonBg('var(--successGreen)')
-        }, 2000)
-    }
-
     useEffect(() => {
         setButtonBg('var(--lightBlue)');
         setButtonVal('Send');
     }, [focus]);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            Name: name,
+            Email: email,
+            Phone: phone,
+            Note: note,
+            Date: new Date(),
+        };
+        setButtonVal('...');
+
+        fetch("https://sheet.best/api/sheets/7553d114-1ff7-4ea3-a09d-397bb1238976", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((r) => r.json())
+        .then((data) => {
+            // The response comes here
+            console.log(data);
+            setName('');
+            setEmail('');
+            setPhone('');
+            setNote('');
+            setButtonVal('Message sent!');
+            setButtonBg('var(--successGreen)');
+        })
+        .catch((error) => {
+            console.log(error);
+            setName('');
+            setEmail('');
+            setPhone('');
+            setNote('');
+            setButtonVal('Error!');
+            setButtonBg('var(--failureRed)');
+        });
+
+        
+      
+       
+    }
+
     return (
 
         <motion.form
-            className="contact-form contact-card" name="contact" method="post">
-            <input type="hidden" name="contactform" value="contact" />
+            className="contact-form contact-card">
             <h4>Message Me</h4>
             <motion.label layout htmlFor="name"><input value={name} onChange={e => setName(e.target.value)} onFocus={() => setFocus("name")} onBlur={() => setFocus(null)} name="name" type="text" /> <motion.span layout="position" className={focus === "name" || name !== "" ? "contact-form-span-active" : ""} style={focus !== 'name' ? { color: 'var(--white)' } : {}}>Name</motion.span> </motion.label>
             <motion.label layout htmlFor="email"><input value={email} onChange={e => setEmail(e.target.value)} onFocus={() => setFocus("email")} onBlur={() => setFocus(null)} name="email" type="text" /><motion.span layout="position" className={focus === "email" || email !== "" ? "contact-form-span-active" : ""} style={focus !== 'email' ? { color: 'var(--white)' } : {}}>Email</motion.span></motion.label>
             <motion.label layout htmlFor="phone"><input value={phone} onChange={e => setPhone(e.target.value)} onFocus={() => setFocus("phone")} onBlur={() => setFocus(null)} name="phone" type="text" /><motion.span layout="position" className={focus === "phone" || phone !== "" ? "contact-form-span-active" : ""} style={focus !== 'phone' ? { color: 'var(--white)' } : {}}>Phone</motion.span></motion.label>
             <motion.label style={focus === "note" || note !== "" ? { height: "auto" } : {}} layout htmlFor="note"><textarea value={note} rows={focus === 'note' || note !== "" ? 5 : 1} onChange={e => setNote(e.target.value)} onFocus={() => setFocus("note")} onBlur={() => setFocus(null)} name="note" /><motion.span layout="position" className={focus === "note" || note !== "" ? "contact-form-span-active" : ""} style={focus !== 'note' ? { color: 'var(--white)' } : {}}>Note</motion.span></motion.label>
-            <motion.input layout style={{ background: buttonBg }} value={buttonVal} type="submit" disabled={buttonVal === "Send" ? false : true} />
+            <motion.input layout style={{ background: buttonBg }} value={buttonVal} type="submit" onClick={handleSubmit} disabled={buttonVal === "Send" ? false : true} />
             {buttonBg === 'var(--successGreen)' && <p> Thanks for your message! I'll get back to you!</p>}
+            {buttonBg === 'var(--failureRed)' && <p> There was an error sending your message :( Please contact me directly via my contact detals above, thanks!</p>}
         </motion.form>
 
 
